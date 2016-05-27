44e542f6754a7d2047b90e89f3d6c40148242920
Ryan Carlile <ryancarlile@gmail.com>
2016-05-27 11:39:23 -0700

    refactored weather app

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -2,15 +2,15 @@ var Weather = require('./weather.js').Weather;
 
 var apiKey = require('./../.env').apiKey;
 
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
+    currentWeatherObject.getWeather(city, displayHumidity);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,12 @@
+var apiKey = require('./../.env').apiKey;
 
+exports.Weather = function(){
+}
+
+exports.Weather.prototype.getWeather = function(city) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    return response.main.humidity;
+  }).fail(function(error) {
+    $('.showWeather').text(error.responseJSON.message);
+  });
+}
